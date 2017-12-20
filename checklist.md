6fbf15c336c7f95a7708a36100e85e7d00688921
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-19 20:56:29 -0700

    Added Admin can delete a category

--- checklist.md
+++ checklist.md
@@ -22,7 +22,7 @@ Authentication and Authorization
 [x] Users need to log in to see their ideas.
 [ ] Users can only see their own ideas – they should not be able to visit another user’s page.
 [ ] Users cannot create ideas for other users.
-[ ] Users cannot create new categories – only the admin can do that.
+[x] Users cannot create new categories – only the admin can do that.
 [ ] Users cannot create images – only the admin can do that; however, a user can assign an image to their idea.
 [x] Visitors can create user accounts.
 
--- spec/features/categories/admin_can_destroy_a_category_spec.rb
+++ spec/features/categories/admin_can_destroy_a_category_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 
 describe "when an admin visits the categories index page" do
   it "allows for them to delete a category" do
-    skip
+    admin = User.create!(username: "Lil B", password: "basedgod", role: 1)
+    Category.create!(title: "Great")
     categories = Category.all
-    category = Category.create!(title: "Great")
-    visit categories_path
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit admin_categories_path
 
     expect(categories.count).to eq(1)
     click_on "Delete"
 
-    expect(current_path).to eq(categories_path)
+    expect(current_path).to eq(admin_categories_path)
     expect(categories.count).to eq(0)
   end
 end
