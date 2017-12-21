a7d32285723bd99025bfd895129c56721e1da515
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-21 00:28:27 -0700

    Added admin can delete category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,18 +1,12 @@
 class Admin::CategoriesController < Admin::BaseController
 
-  # def index
-  #   @user = current_user
-  #   @categories = Category.all
-  #   @category = Category.new
-  # end
-
   def create
     @category = Category.new(category_params)
     if @category.save
       flash[:notice] = "Category named #{@category.title} created!"
       redirect_to admin_dashboard_path
     else
-      flash[:notice] = "There was an error creating your category, I don't know what it is but fix that shit b"
+      flash[:notice] = "There was an error creating your category"
     end
   end
 
--- db/migrate/20171218005148_add_category_ref_to_ideas.rb
+++ db/migrate/20171218005148_add_category_ref_to_ideas.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefToIdeas < ActiveRecord::Migration[5.1]
   def change
-    add_reference :ideas, :category, foreign_key: true
+    add_reference :ideas, :category, foreign_key: true, on_delete: :cascade
   end
 end
